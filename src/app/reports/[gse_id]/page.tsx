3e54dc508cbebfeeec2124b2c5eb4d7d79d16456
Daniel J. B. Clarke <u8sand@gmail.com>
2025-07-18 14:13:28 -0400

    Show loading/error indicators

--- src/app/reports/[gse_id]/page.tsx
+++ src/app/reports/[gse_id]/page.tsx
@@ -4,30 +4,17 @@ import React from 'react';
 
 export default function ReportPage(props: { params: Promise<{ gse_id: string }> }) {
   const params = React.use(props.params)
-  //const [html, setHtml] = useState('');
-
-  // useEffect(() => {
-  //   fetch(`/${gse_id}/${gse_id}.html`)
-  //     .then(res => res.text())
-  //     .then(data => setHtml(data))
-  //     .catch(err => console.error(err));
-  // }, [gse_id]); 
-
-  const { data: html = '<span>No results</span>' } = trpc.getHTML.useQuery({ id: params.gse_id });
-  // const fetch_html = async (id: string) => {
-  //   const html = trpc.getHTML.useQuery({ id });
-  //   if (html) return (html)
-  //     else return ('<span>No results</span>')
-  // }
-
+  const { data: html, error, isLoading } = trpc.getHTML.useQuery({ id: params.gse_id }, { retry: false });
 
   return (
     <div className="notebook-content">
       <main className="flex-grow p-4">
-        <div
+        {error && <div>Error: {error.message}</div>}
+        {isLoading && <div>Loading...</div>}
+        {html && <div
           className="prose max-w-none"
           dangerouslySetInnerHTML={{ __html: html }} 
-        />
+        />}
       </main>
     </div>
   );
